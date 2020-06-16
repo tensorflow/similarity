a5299105c00bd2f814b7b15b796fb4e13db983a5
Shun Lin <shunlin@google.com>
2020-06-15 17:25:12 -0700

    adding license header to files

--- tensorflow_similarity/visualization/distance_histograms.py
+++ tensorflow_similarity/visualization/distance_histograms.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import math
 from collections import defaultdict
 
@@ -106,8 +120,16 @@ def plot_distance_histograms(
         # use different coor for "All Samples" subplot to indicate that this
         # subplot contains aggregated data
         if key == all_samples_key:
-            ax.hist(value["correct"], alpha=0.6, color="green", weights=weights)
-            ax.hist(value["incorrect"], alpha=0.8, color="red", weights=weights)
+            ax.hist(
+                value["correct"],
+                alpha=0.6,
+                color="green",
+                weights=weights)
+            ax.hist(
+                value["incorrect"],
+                alpha=0.8,
+                color="red",
+                weights=weights)
         else:
             # use blue and orange for accessibility (color-blindness)
             ax.hist(value["correct"], alpha=0.6, color="blue", weights=weights)
