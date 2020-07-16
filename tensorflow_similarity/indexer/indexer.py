7d02628249ad6fa1da7f981516076ea3ecf4bc37
dominikschmidt <dominikschmidt1893@gmail.com>
2020-07-16 16:13:13 -0700

    Added files and tests for indexer, created skeleton for indexer and cl tool.

--- tensorflow_similarity/indexer/indexer.py
+++ tensorflow_similarity/indexer/indexer.py
@@ -12,6 +12,82 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Indexer:
-    def __init__():
+import nmslib
+import tensorflow as tf
+
+class Indexer(object):
+    """ Indexer class that indexes Embeddings. This allows for efficient
+        searching of approximate nearest neighbors for a given embedding
+        in metric space.
+    """
+
+    def __init__(self, dataset, model_path, index_dir):
+        self.model = tf.keras.models.load_model(model_path, custom_objects={'tf': tf})
+        self.dataset = dataset
+        self.index_dir = index_dir
+        self.index = nmslib.init(method='hnsw', space='cosinesimil')
+        self.thresholds = dict()
+
+    """ build an index from a dataset 
+    """
+    def build():
+        # TODO
+        pass
+
+
+    """ find the closest data points and their associated data in the index
+
+        Args:
+            item (Item): The item for a which a query of the most similar items should be performed
+            num_neighbors (int): The number of neighbors that should be returned
+
+        Returns:
+            neighbors (list(Item)): A list of the nearest neighbor items
+    """
+    def find(item, num_neighbors):
+        # TODO
+        pass
+
+    """ Store an indexer to the disk
+    """
+    def save(path):
+        # TODO
+        pass
+
+    """ Load an indexer from the disk
+    """
+    def load(path):
+        # TODO
+        pass
+    
+
+    """ Add an item to the index
+        Args:
+            item (Item): The item to be added to the index
+    """
+    def add(item):
+        # TODO
+        pass
+
+
+     """ Remove an item from the index
+        Args:
+            item (Item): The item to removed added to the index
+    """
+    def remove(item):
+        # TODO
+        pass
+
+
+    """ Rebuild the index after updates were made
+    """
+    def rebuild():
+        # TODO
+        pass
+
+
+    """ Compute thresholds for similarity using V measure score
+    """
+    def compute_thresholds():
+        # TODO
         pass
---
+++ tests/tensorflow_similarity/indexer/test.py
@@ -0,0 +1,45 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+from tensorflow_similarity.indexer.indexer import Indexer
+
+class ServingTestCase(unittest.TestCase):
+    def test_indexer_constructor(self):
+        self.assertTrue(true)
+
+    def test_indexer_build(self):
+        self.assertTrue(true)
+
+    def test_indexer_find(self):
+        self.assertTrue(true)
+
+    def test_indexer_add(self):
+        self.assertTrue(true)
+
+    def test_indexer_remove(self):
+        self.assertTrue(true)
+
+    def test_indexer_save(self):
+        self.assertTrue(true)
+
+    def test_indexer_load(self):
+        self.assertTrue(true)
+
+    def test_indexer_rebuild(self):
+        self.assertTrue(true)
+
+    def test_indexer_compute_thresholds(self):
+        self.assertTrue(true)
