92d33c522d9ebda479bad6863bd7590d975df007
Dominik Schmidt <dominikschmidt1893@gmail.com>
2020-07-21 17:11:04 -0700

    Refactor indexer (#19)
    * Added files for indexer
    
    * Added files and tests for indexer, created skeleton for indexer and cl tool.
    
    * Update
    
    * Push
    
    * Update
    
    * moved tests, switched to pytest
    
    * Update
    
    * Added setup script
    
    * switched to config.json
    
    * remove import
    
    * Updated docstring
    
    * Added link in doc string

---
+++ tensorflow_similarity/indexer/indexer.py
@@ -0,0 +1,99 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import nmslib
+import tensorflow as tf
+
+class Indexer(object):
+    """ Indexer class that indexes Embeddings. This allows for efficient
+        searching of approximate nearest neighbors for a given embedding
+        in metric space.
+
+        Args:
+            model_path (string): The path to the model that should be used to calculate embeddings
+            dataset (Dataset/Tf.Dataset?): The dataset to be indexed
+            index_dir (string): The path to the directory where the indexer should be saved,
+            space (string): The space (a space is a combination of data and the distance) to use in the indexer
+                            for a list of available spaces see: https://github.com/nmslib/nmslib/blob/master/manual/spaces.md
+    """
+
+    def __init__(self, dataset, model_path, index_dir, space="cosinesimil"):
+        self.model = tf.keras.models.load_model(model_path, custom_objects={'tf': tf})
+        self.dataset = dataset
+        self.index_dir = index_dir
+        self.index = nmslib.init(method='hnsw', space=space)
+        self.thresholds = dict()
+
+    def build():
+        """ build an index from a dataset 
+        """
+        # TODO
+        pass
+
+    def find(item, num_neighbors):
+        """ find the closest data points and their associated data in the index
+
+            Args:
+                item (Item): The item for a which a query of the most similar items should be performed
+                num_neighbors (int): The number of neighbors that should be returned
+
+            Returns:
+                neighbors (list(Item)): A list of the nearest neighbor items
+        """
+        # TODO
+        pass
+
+    def save():
+        """ Store an indexer on the disk
+        """
+        # TODO
+        pass
+
+    def load(path):
+        """ Load an indexer from the disk
+
+            Args:
+                The path to the file that the indexer is be loaded from
+        """
+        # TODO
+        pass
+    
+    def add(item):
+        """ Add an item to the index
+        
+            Args:
+                item (Item): The item to be added to the index
+        """
+        # TODO
+        pass
+
+    def remove(item):
+        """ Remove an item from the index
+            Args:
+                item (Item): The item to removed added to the index
+        """
+        # TODO
+        pass
+
+    def rebuild():
+        """ Rebuild the index after updates were made
+        """
+        # TODO
+        pass
+
+    def compute_thresholds():
+        """ Compute thresholds for similarity using V measure score
+        """
+        # TODO
+        pass
