73a4881f907c2cbc80b6482ff84afc2df517bb1d
Elie Bursztein <github@elie.net>
2020-11-11 12:05:10 -0800

    statistics in indexer and code simplificcation

--- tensorflow_similarity/indexer.py
+++ tensorflow_similarity/indexer.py
@@ -1,9 +1,15 @@
 "Index embedding to allow distance based lookup"
 
 import nmslib
+from time import time
+from collections import defaultdict
+from collections import deque
+from tabulate import tabulate
+
 from .metrics import metric_name_canonializer
 from .mappers import MemoryMapper
 from tqdm.auto import tqdm
+import numpy as np
 
 
 class Indexer():
@@ -12,7 +18,8 @@ class Indexer():
                  distance='cosine',
                  metadata={},
                  mapper='memory',
-                 method='hnsw'):
+                 method='hnsw',
+                 stat_buffer_size=100):
 
         self.metadata = metadata
 
@@ -29,17 +36,34 @@ class Indexer():
         else:
             self.mapper = mapper
 
+        # fast ANN index
+        # FIXME: rename matcher
         self.index = nmslib.init(method=method, space=self.space_name)
 
-    def add(self, embedding, label=None, data=None):
+        # stats
+        self._stats = defaultdict(int)
+        self.stat_buffer_size = stat_buffer_size
+        # bound timing buffers
+        self.lookup_timings = deque([], maxlen=self.stat_buffer_size)
+
+    def add(self, embedding, label=None, data=None, build=True, verbose=1):
         """ Add a single embedding to the indexer
 
         Args:
             embedding (tensor): TF similarity model output / embeddings.
-            label (str/int, optional): label(s) associated with the
+
+            label (str/int, optional): Label(s) associated with the
             embedding. Defaults to None.
-            data (Tensor, optional): input data associated with the embedding.
+
+            data (Tensor, optional): Input data associated with the embedding.
             Defaults to None.
+
+            build (bool, optional): Rebuild the index after insertion.
+            Defaults to True. Set it to false if you would like to add
+            multiples batchs/points and build it manually once after.
+
+            verbose (int, optional): Display progress if set to 1.
+            Defaults to 1.
         """
         # store data and get its id
         idx = self._store_data(embedding, label, data)
@@ -49,10 +73,14 @@ class Indexer():
         # !are inverted
         self.index.addDataPoint(idx, embedding)
 
+        if build:
+            self.build(verbose=verbose)
+
     def batch_add(self,
                   embeddings,
                   labels=None,
                   data=None,
+                  build=True,
                   verbose=1):
         """Add a batch of embeddings to the indexer
 
@@ -62,8 +90,13 @@ class Indexer():
             embedding. Defaults to None.
             datas (list(Tensor), optional): input data associated with the
             embedding. Defaults to None.
-            verbose (bool, optional): display progress bar as
-            indexing is taking place. Defaults to 1.
+
+            build (bool, optional): Rebuild the index after insertion.
+            Defaults to True. Set it to false if you would like to add
+            multiples batchs/points and build it manually once after.
+
+            verbose (int, optional): Display progress if set to 1.
+            Defaults to 1.
         """
 
         # store points
@@ -96,10 +129,17 @@ class Indexer():
         if verbose:
             pb.close()
 
+        if build:
+            self.build(verbose=verbose)
+
     def build(self, verbose=1):
-        """Build the indexer
+        """Build the index this is need to take into account the new points
         """
-        self.index.createIndex(print_progress=verbose)
+        show = True if verbose else False
+        self.index.createIndex(print_progress=show)
+        self._stats['query_time'] = 0
+        self._stats['query'] = 0
+        self.lookup_timing = deque([], maxlen=self.stat_buffer_size)
 
     def lookup(self, embedding, k=5):
         """Find the k closest match of a given embedding
@@ -112,11 +152,16 @@ class Indexer():
         """
 
         results = []
+        start = time()
         idxs, distances = self.index.knnQuery(embedding, k=k)
         for i, idx in enumerate(idxs):
             data = self.mapper.get(idx)
             data['distance'] = distances[i]
             results.append(data)
+        lookup_time = time() - start
+        self.lookup_timings.append(lookup_time)
+        self._stats['num_lookups'] += 1
+
         return results
 
     def batch_lookup(self, embedding, k=5):
@@ -152,3 +197,38 @@ class Indexer():
         "store data using mapper and assign it an id"
         data = {"embedding": embedding, 'label': label, 'data': data}
         return self.mapper.add(data)
+
+    def stats(self):
+        """return index statistics"""
+        stats = self._stats
+        stats['num_items'] = self.mapper.size()
+        stats['stat_buffer_size'] = self.stat_buffer_size
+
+        # query performance - make sure we don't count unused buffer
+        max_idx = min(stats['num_lookups'], self.stat_buffer_size)
+        lookup_timings = list(self.lookup_timings)[:max_idx]
+        stats['query_performance'] = {
+            'min': np.min(lookup_timings),
+            'max': np.max(lookup_timings),
+            'avg': np.average(lookup_timings),
+            'median': np.median(lookup_timings),
+            'stddev': np.std(lookup_timings)
+        }
+        return stats
+
+    def print_stats(self):
+        "display statistics in terminal friendly fashion"
+        stats = self.stats()
+
+        rows = []
+        for k, v in stats.items():
+            if not isinstance(v, dict):
+                rows.append([k, v])
+        print('[Index statistics]')
+        print(tabulate(rows))
+
+        print('\n[Query performance]')
+        rows = []
+        for k, v in stats['query_performance'].items():
+            rows.append([k, v])
+        print(tabulate(rows))
--- tensorflow_similarity/mappers.py
+++ tensorflow_similarity/mappers.py
@@ -1,8 +1,11 @@
 class MemoryMapper():
 
     def __init__(self):
-        self.num_references = 0
-        self.htable = {}  # potentially use an array if we don't include a del.
+
+        # We are using an array for scalability and speed.
+        # Makes del a massive pain but its infrequent
+        self.htable = []
+        self.num_items = 0
 
     def add(self, data):
         """Add a value to the mapper
@@ -13,9 +16,9 @@ class MemoryMapper():
         Returns:
             int: associated id
         """
-        idx = self.num_references
-        self.htable[idx] = data
-        self.num_references += 1
+        idx = self.num_items
+        self.htable.append(data)
+        self.num_items += 1
         return idx
 
     def get(self, idx):
@@ -29,6 +32,10 @@ class MemoryMapper():
         """
         return self.htable[idx]
 
+    def size(self):
+        "Number of elements in the mapper"
+        return self.num_items
+
     def delete(self, idx):
         """Remove point from the mapper"""
-        del self.data[idx]
+        raise NotImplementedError
