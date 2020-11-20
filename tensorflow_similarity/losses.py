7f637af4ad720a579109886d56becd1924a0471c
Elie Bursztein <github@elie.net>
2020-11-20 15:21:01 -0800

    alpha version working in colab

--- tensorflow_similarity/losses.py
+++ tensorflow_similarity/losses.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 import tensorflow as tf
-from tensorflow_addons.utils.keras_utils import LossFunctionWrapper
 from .distances import metric_name_canonializer, pairwise_cosine
 
 """
@@ -173,6 +172,50 @@ def triplet_loss(labels, embeddings, distance='cosine',
 
     triplet_loss = tf.reduce_mean(triplet_loss)
     return triplet_loss
+
+
+class LossFunctionWrapper(tf.keras.losses.Loss):
+    """Wraps a loss function in the `Loss` class."""
+
+    def __init__(
+        self, fn, reduction=tf.keras.losses.Reduction.AUTO, name=None, **kwargs
+    ):
+        """Initializes `LossFunctionWrapper` class.
+        Args:
+          fn: The loss function to wrap, with signature `fn(y_true, y_pred,
+            **kwargs)`.
+          reduction: (Optional) Type of `tf.keras.losses.Reduction` to apply to
+            loss. Default value is `AUTO`. `AUTO` indicates that the reduction
+            option will be determined by the usage context. For almost all cases
+            this defaults to `SUM_OVER_BATCH_SIZE`. When used with
+            `tf.distribute.Strategy`, outside of built-in training loops such as
+            `tf.keras` `compile` and `fit`, using `AUTO` or `SUM_OVER_BATCH_SIZE`
+            will raise an error. Please see this custom training [tutorial](
+              https://www.tensorflow.org/tutorials/distribute/custom_training)
+            for more details.
+          name: (Optional) name for the loss.
+          **kwargs: The keyword arguments that are passed on to `fn`.
+        """
+        super().__init__(reduction=reduction, name=name)
+        self.fn = fn
+        self._fn_kwargs = kwargs
+
+    def call(self, y_true, y_pred):
+        """Invokes the `LossFunctionWrapper` instance.
+        Args:
+          y_true: Ground truth values.
+          y_pred: The predicted values.
+        Returns:
+          Loss values per sample.
+        """
+        return self.fn(y_true, y_pred, **self._fn_kwargs)
+
+    def get_config(self):
+        config = {}
+        for k, v in iter(self._fn_kwargs.items()):
+            config[k] = tf.keras.backend.eval(v) if is_tensor_or_variable(v) else v
+        base_config = super().get_config()
+        return {**base_config, **config}
 
 
 @tf.keras.utils.register_keras_serializable(package="Similarity")
