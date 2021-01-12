f0a757567ad1e3da7f3f78687b7e93c616cac67b
Owen S Vallis <owensvallis@gmail.com>
2021-01-11 21:50:06 -0800

    Fix version numbers.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from time import time
 
 long_description = open("README.md").read()
 # version = '0.15.3r%s' % int(time())
-version = '0.16.0'
+version = '0.16.1'
 
 setup(name="tensorflow_similarity_alpha",
       version=version,
