4ed314bff6a1b9542f2ec8bfe0fcad01433523a1
Elie Bursztein <github@elie.net>
2021-02-12 20:56:06 -0800

    metrics done

--- tensorflow_similarity/metrics.py
+++ tensorflow_similarity/metrics.py
@@ -1,78 +1,281 @@
+from abc import abstractmethod
 import tensorflow as tf
+from typing import List, Union
 
 
-def true_positives(y_true, y_pred):
-    """Calculates the number of true positives.
+class EvalMetric():
 
-    Args:
-        y_true (list(int)): array if class as int.
-        y_pred (list(int)): array of class as int
+    def __init__(self, direction: str, name: str) -> None:
+        self.name = name
+        self.direction = direction
 
-    Returns:
-        int: num true positives
+    @abstractmethod
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> Union[int, float]:
+        pass
+        # match_ranks: rank 0 is unmatched, rank1 is first neighbopor, rank2 ...
+        # match_distance: distance 0 means infinite, rest is match distances
+
+    def _suffix_name(self, name: str, k: int = None, distance: float = None) -> str:
+        "Suffix metric name with k and distance if needed"
+        if k and k > 1:
+            name = "%s@%d" % (name, k)
+
+        if distance and distance != 0.5:
+            name = "%s:%f" % (name, distance)
+
+        return name
+
+    def filter_ranks(self,
+                     match_ranks: List[int],
+                     match_distances: List[float],
+                     min_rank: int = 1,
+                     max_rank: int = None,
+                     distance: float = None) -> List[int]:
+        """Filter match ranks to only keep matches between `min_rank`
+        and `max_rank` below a give distance.
+
+        Args:
+            match_ranks (List[int]): [description]
+            min_rank (int, optional): Minimal rank to keep (inclusive).
+            Defaults to 1.
+
+            max_rank (int, optional): Max rank to keep (inclusive).
+            Defaults to None. If None will keep all ranks above `min_rank`
+
+        Returns:
+            List[int]: filtered ranks as a dense array with missing elements
+            removed. len(filtered_ranks) <= len(match_ranks)
+        """
+        filtered_ranks = []
+        if not max_rank:
+            max_rank = max(match_ranks)
+
+        if not distance:
+            distance = max(match_distances)
+
+        for idx, r in enumerate(match_ranks):
+            if min_rank <= r <= max_rank and match_distances[idx] <= distance:
+                filtered_ranks.append(r)
+        return filtered_ranks
+
+
+class MeanRank(EvalMetric):
+
+    def __init__(self, name='mean_rank') -> None:
+        super().__init__(direction='min', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> float:
+
+        # remove unmatched elements (rank 0)
+        matches = self.filter_ranks(match_ranks, match_distances, max_rank=max_k)
+        return float(tf.reduce_mean(matches))
+
+
+class MinRank(EvalMetric):
+
+    def __init__(self, name='min_rank') -> None:
+        super().__init__(direction='min', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> int:
+
+        # remove unmatched elements (rank 0)
+        matches = self.filter_ranks(match_ranks, match_distances, max_rank=max_k)
+        print(matches)
+        return int(tf.reduce_min(matches))
+
+
+class MaxRank(EvalMetric):
+
+    def __init__(self, name='max_rank') -> None:
+        super().__init__(direction='min', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> int:
+
+        # remove unmatched elements (rank 0)
+        matches = self.filter_ranks(match_ranks, match_distances, max_rank=max_k)
+        return int(tf.reduce_max(matches))
+
+
+class Accuracy(EvalMetric):
+    """How many correct matches are returned for the given paramters
+    probably the most important metric. Accuracy can be at k=1 when
     """
-    tp = tf.cast(tf.math.equal(y_true, y_pred), 'int32')
-    return tf.cast(tf.reduce_sum(tp), 'float32')
+
+    def __init__(self,
+                 distance_threshold: float = 0.5,
+                 k: int = 1,
+                 name='accuracy') -> None:
+        self.k = k
+        self.distance_threshold = distance_threshold
+        name = self._suffix_name(name, k, distance_threshold)
+        super().__init__(direction='max', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> float:
+
+        matches = self.filter_ranks(match_ranks,
+                                    match_distances,
+                                    max_rank=self.k,
+                                    distance=self.distance_threshold)
+
+        return len(matches) / len(targets_labels)
 
 
-def false_positives(y_true, y_pred):
-    """Calculates the number of false positives.
+class Precision(EvalMetric):
+    """ Compute the precision of the matches.
 
-    Args:
-        y_true (list(int)): array if class as int.
-        y_pred (list(int)): array of class as int
+    Notes: precision computation for similarity is different from
+    the classic version and also tricky to get right so please refers
+    to the tf.similarity paper for a complete discussion of how it works.
 
-    Returns:
-        int: num true positives
+    Precision formula: `true_positives / (true_positives + false_positives)`
+
+
+    Reference: TBD
     """
-    tp = tf.cast(tf.math.not_equal(y_true, y_pred), 'int32')
-    return tf.cast(tf.reduce_sum(tp), 'float32')
+
+    def __init__(self,
+                 distance_threshold: float = 0.5,
+                 k: int = 1,
+                 name='precision') -> None:
+        self.k = k
+        self.distance_threshold = distance_threshold
+        name = self._suffix_name(name, k, distance_threshold)
+        super().__init__(direction='max', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> float:
+
+        matches = self.filter_ranks(match_ranks,
+                                    match_distances,
+                                    max_rank=self.k,
+                                    distance=self.distance_threshold)
+
+        true_positives = len(matches)
+        false_positives = num_matched - true_positives
+        return true_positives / (true_positives + false_positives)
 
 
-def accuracy(y_true, y_pred):
-    tp = true_positives(y_true, y_pred)
-    return tp / len(y_true)
+class Recall(EvalMetric):
+    """Computing matcher recall at k for a given distance threshold
 
-
-def precision(y_true, y_pred):
-    tp = true_positives(y_true, y_pred)
-    fp = false_positives(y_true, y_pred)
-    return (tp / (tp + fp))
-
-
-def recall(y_true, y_pred):
-    tp = true_positives(y_true, y_pred)
-    return (tp / len(y_true))
-
-
-def f1_score(y_true, y_preds):
-    """Compute the F1 score, also known as balanced F-score or F-measure
-
-  https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
-
-    Args:
-        y_true (1D tensor(int)): ground truth.
-        y_pred (1D tensor(int)): Estimated targets as returned by a classifier.
-
-    Returns:
-        float: f1 score
+    Recall formula: `true_positive / (true_positive + false_negative)`
     """
-    pr = precision(y_true, y_preds)
-    re = recall(y_true, y_preds)
-    return 2 * (pr * re) / (pr + re)
+
+    def __init__(self,
+                 distance_threshold: float = 0.5,
+                 k: int = 1,
+                 name='recall') -> None:
+        self.k = k
+        self.distance_threshold = distance_threshold
+        name = self._suffix_name(name, k, distance_threshold)
+        super().__init__(direction='max', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> float:
+
+        matches = self.filter_ranks(match_ranks,
+                                    match_distances,
+                                    max_rank=self.k,
+                                    distance=self.distance_threshold)
+
+        true_positives = len(matches)
+        # targets_labels = true positive + false negative
+        return true_positives / len(targets_labels)
 
 
-def fast_f1_score(precision, recall):
-    """Compute the F1 score, also known as balanced F-score or F-measure
-    when precision and recall are already computed
+class F1Score(EvalMetric):
+    """Compute the F1 score, also known as balanced F-score or F-measure at K
+    below a distance threshold.
 
     https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
 
-    Args:
-        precision (float): Precision metric
-        recall (float): Precision metric
 
-    Returns:
-        float: f1 score
+    f1_score formula is: `2 * (precision * recall) / (precision + recall)`
+
     """
-    return 2 * (precision * recall) / (precision + recall)
+
+    def __init__(self,
+                 distance_threshold: float = 0.5,
+                 k: int = 1,
+                 name='f1_score') -> None:
+        self.k = k
+        self.distance_threshold = distance_threshold
+        name = self._suffix_name(name, k, distance_threshold)
+        super().__init__(direction='max', name=name)
+
+    def compute(self,
+                max_k: int,
+                targets_labels: List[int],
+                num_matched: int,
+                num_unmatched: int,
+                index_size: int,
+                match_ranks: List[int],
+                match_distances: List[float],
+                matches_labels: List[List[int]]) -> float:
+
+        matches = self.filter_ranks(match_ranks,
+                                    match_distances,
+                                    max_rank=self.k,
+                                    distance=self.distance_threshold)
+
+        true_positives = len(matches)
+        false_positives = num_matched - true_positives
+
+        precision = true_positives / (true_positives + false_positives)
+        recall = true_positives / len(targets_labels)
+
+        return 2 * (precision * recall) / (precision + recall)
