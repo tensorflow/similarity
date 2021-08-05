0c30192dde9a9271386df4d150df6ebdcfb606f6
Elie Bursztein <github@elie.net>
2021-08-04 17:25:06 -0700

    Samplers refactoring

---
+++ tests/samplers/test_tfdataset_samplers.py
@@ -0,0 +1,24 @@
+from tensorflow_similarity.samplers import TFDatasetMultiShotMemorySampler
+import pytest
+
+
+def test_basic():
+    dataset_name = "mnist"
+    sampler = TFDatasetMultiShotMemorySampler(dataset_name=dataset_name,
+                                              class_per_batch=10)
+    batch = sampler.generate_batch(42)
+    assert batch[0].shape == (20, 28, 28, 1)
+
+def test_wrong_key():
+    dataset_name = "mnist"
+
+    # X
+    with pytest.raises(ValueError):
+        TFDatasetMultiShotMemorySampler(dataset_name=dataset_name,
+                                        class_per_batch=4,
+                                        x_key='error')
+    # Y
+    with pytest.raises(ValueError):
+        TFDatasetMultiShotMemorySampler(dataset_name=dataset_name,
+                                        class_per_batch=4,
+                                        y_key='error')
