844a28835e8711860255d1c9d3d849be547e6f5d
Owen Vallis <owensvallis@gmail.com>
2021-08-12 19:45:21 +0000

    Remove lifted_struct_loss from losses init

--- tensorflow_similarity/losses/__init__.py
+++ tensorflow_similarity/losses/__init__.py
@@ -2,5 +2,4 @@ from .pn_loss import PNLoss  # noqa
 from .triplet_loss import TripletLoss  # noqa
 from .metric_loss import MetricLoss  # noqa
 from .circle_loss import CircleLoss  # noqa
-from .lifted_struct_loss import LiftedStructLoss  # noqa
 from .multisim_loss import MultiSimilarityLoss  # noqa
