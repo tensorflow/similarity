d05bfac7f22a0e403981808fb4dfd719bb2a9c75
Owen Vallis <owensvallis@gmail.com>
2021-08-27 22:55:11 +0000

    Update callbacks to call evaluator directly.

--- tensorflow_similarity/callbacks.py
+++ tensorflow_similarity/callbacks.py
@@ -1,14 +1,16 @@
-from typing import List, Sequence, Union
+from typing import Dict, List, Sequence, Union
 from pathlib import Path
 import math
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
 
 from .classification_metrics import ClassificationMetric
 from .classification_metrics import make_classification_metric # noqa
-from .evaluators import MemoryEvaluator
-from .types import Tensor, FloatTensor, IntTensor
+from .evaluators import Evaluator, MemoryEvaluator
+from .models import SimilarityModel
+from .types import Tensor, IntTensor
 from .utils import unpack_lookup_distances, unpack_lookup_labels
 
 
@@ -86,24 +88,20 @@ class EvalCallback(Callback):
         # rebuild the index
         self.model.index(self.targets, self.target_labels, verbose=0)
 
-        lookups = self.model.lookup(self.queries, verbose=0)
-        lookup_distances = unpack_lookup_distances(lookups)
-        lookup_labels = unpack_lookup_labels(lookups)
-
-        results = self.evaluator.evaluate_classification(
-            query_labels=self.query_labels,
-            lookup_labels=lookup_labels,
-            lookup_distances=lookup_distances,
-            distance_thresholds=tf.constant([math.inf]),
-            metrics=self.metrics,
-            matcher='match_nearest',
-            verbose=0)
+        results = _evaluate_classification(
+                queries=self.queries,
+                query_labels=self.query_labels,
+                model=self.model,
+                evaluator=self.evaluator,
+                k=self.k,
+                metrics=self.metrics
+        )
 
         mstr = []
         for metric_name, vals in results.items():
             float_val = vals[0]
             logs[metric_name] = float_val
-            mstr.append(f'{metric_name}:{float_val:0.4f}')
+            mstr.append(f'{metric_name}: {float_val:.4f}')
             if self.tb_writer:
                 with self.tb_writer.as_default():
                     tf.summary.scalar(metric_name, float_val, step=epoch)
@@ -124,30 +122,64 @@ class SplitValidationLoss(Callback):
     This is useful for separately tracking the validation loss on the seen and
     unseen classes and may provide insight into how well the embedding will
     generalize to new classes.
-
-    Attributes:
-        x_known: The set of examples from the known classes.
-        y_known: The labels associated with the known examples.
-        x_unknown: The set of examples from the unknown classes.
-        y_unknown: The labels associated with the unknown examples.
     """
     def __init__(self,
-                 x: FloatTensor,
-                 y: IntTensor,
-                 known_classes: IntTensor):
+                 queries: Tensor,
+                 query_labels: Sequence[int],
+                 targets: Tensor,
+                 target_labels: Sequence[int],
+                 known_classes: IntTensor,
+                 distance: str = 'cosine',
+                 metrics: Sequence[Union[str, ClassificationMetric]] = ['accuracy', 'f1score'],  # noqa
+                 tb_logdir: str = None,
+                 k=1):
         """Creates the validation callbacks.
 
         Args:
-            x: Validation data.
-            y: Validation labels.
+            queries: Test examples that will be tested against the built index.
+
+            query_labels: Queries nearest neighboors expected labels.
+
+            targets: Examples that are indexed.
+
+            target_labels: Target examples labels.
+
             known_classes: The set of classes seen during training.
+
+            distance: Distance function used to compute pairwise distance
+            between examples embeddings.
+
+            metrics: List of [EvalMetrics](eval_metrics.md) to be computed
+            during the evaluation. Defaults to ['accuracy', 'mean_rank'].
+            embedding to evaluate.
+
+            tb_logdir: Where to write TensorBoard logs. Defaults to None.
+
+            k: How many neigboors to retrive for evaluation. Defaults to 1.
         """
         super().__init__()
+        self.targets = targets
+        self.target_labels = target_labels
+        self.distance = distance
+        self.k = k
+        self.evaluator = MemoryEvaluator()
+        # typing requires this weird formulation of creating a new list
+        self.metrics: List[ClassificationMetric] = (
+                [make_classification_metric(m) for m in metrics])
+
+        if tb_logdir:
+            tb_logdir = str(Path(tb_logdir) / 'index/')
+            self.tb_writer = tf.summary.create_file_writer(tb_logdir)
+            print('TensorBoard logging enable in %s' % tb_logdir)
+        else:
+            self.tb_writer = None
+
+        query_labels = tf.convert_to_tensor(query_labels)
+        query_labels = tf.cast(query_labels, dtype='int32')
 
         # Create separate validation sets for the known and unknown classes
-        y = tf.cast(y, dtype=tf.int32)
-        known_classes = tf.cast(known_classes, dtype=tf.int32)
-        known_classes = tf.squeeze(known_classes)
+        known_classes = tf.cast(known_classes, dtype='int32')
+        known_classes = tf.reshape(known_classes, (-1))
 
         # Use broadcasting to do a y X known_classes equality check. By adding
         # a dim to the start of known_classes and a dim to the end of y, this
@@ -156,32 +188,93 @@ class SplitValidationLoss(Callback):
         # class in known_classes.
         # See https://numpy.org/doc/stable/user/basics.broadcasting.html
         broadcast_classes = tf.expand_dims(known_classes, axis=0)
-        broadcast_labels = tf.expand_dims(y, axis=-1)
+        broadcast_labels = tf.expand_dims(query_labels, axis=-1)
         known_mask = tf.math.reduce_any(
                 broadcast_classes == broadcast_labels, axis=1)
         known_idxs = tf.squeeze(tf.where(known_mask))
         unknown_idxs = tf.squeeze(tf.where(~known_mask))
 
         with tf.device("/cpu:0"):
-            self.x_known = tf.gather(x, indices=known_idxs)
-            self.y_known = tf.gather(y, indices=known_idxs)
+            self.queries_known = tf.gather(queries, indices=known_idxs)
+            self.query_labels_known = (
+                    tf.gather(query_labels, indices=known_idxs))
 
-            self.x_unknown = tf.gather(x, indices=unknown_idxs)
-            self.y_unknown = tf.gather(y, indices=unknown_idxs)
+            self.queries_unknown = tf.gather(queries, indices=unknown_idxs)
+            self.query_labels_unknown = (
+                    tf.gather(query_labels, indices=unknown_idxs))
 
     def on_epoch_end(self, epoch: int, logs: dict = None):
         _ = epoch
         if logs is None:
             logs = {}
-        known_eval = self.model.evaluate_classification(
-                self.x_known,
-                self.y_known,
-                verbose=0)
-        unknown_eval = self.model.evaluate_classification(
-                self.x_unknown,
-                self.y_unknown,
-                verbose=0)
-        print(f'val_los - known_classes: {known_eval:.4f} - '
-              f'unknown_classes: {unknown_eval:.4f}')
-        logs['known_val_loss'] = known_eval
-        logs['unknown_val_loss'] = unknown_eval
+
+        # reset the index
+        self.model.reset_index()
+
+        # rebuild the index
+        self.model.index(self.targets, self.target_labels, verbose=0)
+
+        known_results = _evaluate_classification(
+                queries=self.queries_known,
+                query_labels=self.query_labels_known,
+                model=self.model,
+                evaluator=self.evaluator,
+                k=self.k,
+                metrics=self.metrics
+        )
+
+        unknown_results = _evaluate_classification(
+                queries=self.queries_unknown,
+                query_labels=self.query_labels_unknown,
+                model=self.model,
+                evaluator=self.evaluator,
+                k=self.k,
+                metrics=self.metrics
+        )
+
+        mstr = []
+        for metric_name, vals in known_results.items():
+            float_val = vals[0]
+            full_metric_name = f'{metric_name}_known_classes'
+            logs[full_metric_name] = float_val
+            mstr.append(f'{full_metric_name}: {float_val:0.4f}')
+            if self.tb_writer:
+                with self.tb_writer.as_default():
+                    tf.summary.scalar(full_metric_name, float_val, step=epoch)
+
+        for metric_name, vals in unknown_results.items():
+            float_val = vals[0]
+            full_metric_name = f'{metric_name}_unknown_classes'
+            logs[full_metric_name] = float_val
+            mstr.append(f'{full_metric_name}: {float_val:0.4f}')
+            if self.tb_writer:
+                with self.tb_writer.as_default():
+                    tf.summary.scalar(full_metric_name, float_val, step=epoch)
+
+        print(' - '.join(mstr))
+
+
+def _evaluate_classification(
+                 queries: Tensor,
+                 query_labels: IntTensor,
+                 model: SimilarityModel,
+                 evaluator: Evaluator,
+                 k: int,
+                 metrics: Sequence[ClassificationMetric]
+                 ) -> Dict[str, np.ndarray]:
+    lookups = model.lookup(queries, k=k, verbose=0)
+    lookup_distances = unpack_lookup_distances(lookups)
+    lookup_labels = unpack_lookup_labels(lookups)
+
+    # TODO(ovallis): Support passing other matchers. Currently we are using
+    # match_nearest.
+    results = evaluator.evaluate_classification(
+        query_labels=query_labels,
+        lookup_labels=lookup_labels,
+        lookup_distances=lookup_distances,
+        distance_thresholds=tf.constant([math.inf]),
+        metrics=metrics,
+        matcher='match_nearest',
+        verbose=0)
+
+    return results
