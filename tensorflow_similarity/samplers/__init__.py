50efdae1f379e5ad2fb0bc6dd27cbcb2d9877941
Elie Bursztein <github@elie.net>
2021-08-31 20:05:07 -0700

    Readme (#131)
    * Update the README.
    
    * Fix links in README.
    
    * Update Notebook text.
    
    Co-authored-by: Owen Vallis <ovallis@google.com>

--- tensorflow_similarity/samplers/__init__.py
+++ tensorflow_similarity/samplers/__init__.py
@@ -1,3 +1,10 @@
+"""Data Samplers that generate balanced batches for smooth training.
+
+TensorFlow Similarity provides data samplers for various types of datasets and
+use cases that:
+- Ensure that batches contain at least N examples of each class present in the batch.
+- Support restricting the batches to a subset of the classes present in the dataset.
+"""
 from .utils import select_examples  # noqa
 from .memory_samplers import MultiShotMemorySampler  # noqa
 from .memory_samplers import SingleShotMemorySampler  # noqa
