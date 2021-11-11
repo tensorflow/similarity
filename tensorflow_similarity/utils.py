56ba48bbc51d9ef2576d32150ecad940f15f9053
Owen Vallis <ovallis@google.com>
2021-11-11 00:20:42 +0000

    Unpacking the Lookup objects could fail if the Lookup sets were of different lengths.
    * Add support for loading variable length Lookup sets.
    * Imput label and distance values to make all Lookup sets the same length
    * * The label is set to 0x7FFFFFFF as this is unlikely to be a class label.
    * * The distance is set to math.inf
    * print a warning to the user to notify them that we are imputing the
    values and the number of short Lookup sets.
    * Add tests for all utils.py functions.

--- tensorflow_similarity/utils.py
+++ tensorflow_similarity/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 "Various utilities functions for improved quality of life."
+import math
 from typing import Optional, Sequence
 
 import tensorflow as tf
@@ -28,7 +29,7 @@ def is_tensor_or_variable(x):
 
 def tf_cap_memory():
     "Avoid TF to hog memory before needing it"
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.config.experimental.list_physical_devices("GPU")
 
     if gpus:
         for gpu in gpus:
@@ -40,9 +41,20 @@ def tf_cap_memory():
 
 
 def unpack_lookup_labels(lookups: Sequence[Sequence[Lookup]]) -> IntTensor:
-    # using list comprehension as it is faster
+    # Using list comprehension as it is faster
     all_values = [[n.label for n in lu] for lu in lookups]
-    result: IntTensor = tf.cast(tf.constant(all_values), dtype='int32')
+    # Lookup sets are not guaranteed to all be the same size. Therefore we load
+    # the list of lists as a ragged tensor and convert to an int32 tensor with a
+    # default class label value set to the max value for int32.
+    ragged_labels = tf.ragged.constant(all_values, dtype="int32")
+
+    if not _same_length_rows(ragged_labels):
+        print(f"WARNING: {_count_of_small_lookup_sets(ragged_labels)} lookup "
+              "sets are shorter than the max lookup set length. Imputing "
+              "0x7FFFFFFF for the missing label lookups.")
+
+    result: IntTensor = ragged_labels.to_tensor(default_value=0x7FFFFFFF)
+
     return result
 
 
@@ -51,10 +63,49 @@ def unpack_lookup_distances(
         distance_rounding: Optional[int] = None) -> FloatTensor:
     # using list comprehension as it is faster
     all_values = [[n.distance for n in lu] for lu in lookups]
-    dists: FloatTensor = tf.cast(tf.constant(all_values), dtype='float32')
+    # Lookup sets are not guaranteed to all be the same size. Therefore we load
+    # the list of lists as a ragged tensor and convert to an flat32 tensor with a
+    # default dist value set to math.inf.
+    ragged_dists = tf.ragged.constant(all_values, dtype="float32")
 
     if distance_rounding is not None:
         multiplier = tf.constant([10.0**distance_rounding])
-        dists = tf.round(dists * multiplier) / multiplier
+        ragged_dists = tf.round(ragged_dists * multiplier) / multiplier
+
+    if not _same_length_rows(ragged_dists):
+        print(f"WARNING: {_count_of_small_lookup_sets(ragged_dists)} lookup "
+              "sets are shorter than the max lookup set length. Imputing "
+              "math.inf for the missing distance lookups.")
+
+    dists: FloatTensor = ragged_dists.to_tensor(default_value=math.inf)
 
     return dists
+
+
+def _same_length_rows(x: tf.RaggedTensor) -> bool:
+    """Check if the rows are all the same length.
+
+    Args:
+        x: A RaggedTensor
+
+    Returns:
+       True if all rows are the same length.
+    """
+    dims = tf.expand_dims(x.row_lengths(), axis=-1)
+    pairwise_equality = tf.equal(dims, tf.transpose(dims))
+    return tf.math.reduce_all(pairwise_equality)
+
+
+def _count_of_small_lookup_sets(x: tf.RaggedTensor) -> int:
+    """The count of lookup sets smaller than x.bounding_shape()[1]
+
+    Args:
+        x: A RaggedTensor
+
+    Returns:
+        The count of smaller lookup sets.
+    """
+    rl = x.row_lengths()
+    max_rl = x.bounding_shape()[1]
+    short_lookup_sets = tf.cast(rl != max_rl, dtype="int32")
+    return tf.math.reduce_sum(short_lookup_sets)
