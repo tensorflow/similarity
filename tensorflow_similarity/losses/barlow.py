Fix divide by zero in the barlow column wise normalization and only call barlow loss once for each pair of views.

tensorflow_similarity/losses/barlow.py
@@ -11,8 +11,8 @@ class Barlow(Loss):
     """Barlow Loss"""
 
     def __init__(self,
-                 lambda_: float = 0.005,
-                 margin: float = 10e-8,
+                 lambda_: float = 5e-3,
+                 margin: float = 1e-12,
                  reduction: Callable = tf.keras.losses.Reduction.AUTO,
                  name: Optional[str] = None,
                  **kwargs):
@@ -42,6 +42,7 @@ class Barlow(Loss):
         c = c / tf.cast(batch_size, dtype="float32")
 
         on_diag = 1.0 - tf.linalg.diag_part(c)
+        on_diag = tf.math.pow(on_diag, 2)
         on_diag = tf.math.reduce_sum(on_diag)
 
         off_diag = self.off_diagonal(c)
@@ -50,7 +51,7 @@ class Barlow(Loss):
 
         loss = on_diag + off_diag * self.lambda_
 
-        loss = loss * 0.5 + self.margin
+        loss = loss + self.margin
 
         return loss
 
@@ -70,4 +71,4 @@ class Barlow(Loss):
         col_mean = tf.math.reduce_mean(x, axis=0)
         col_std = tf.math.reduce_std(x, axis=0)
 
-        return (x - col_mean) / col_std
+        return tf.math.divide_no_nan((x - col_mean),  col_std)

tensorflow_similarity/models/contrastive_model.py
@@ -59,16 +59,16 @@ class ContrastiveModel(tf.keras.Model):
             if self.method == "simsiam":
                 h1 = tf.stop_gradient(h1)
                 h2 = tf.stop_gradient(h2)
-                l1_args = (z1, p2)
-                l2_args = (z2, p1)
-            elif self.method in ("simclr", "barlow"):
-                l1_args = (z1, z2)
-                l2_args = (z2, z1)
+                l1 = self.compiled_loss(z1, p2)
+                l2 = self.compiled_loss(z2, p1)
+            elif self.method == "simclr":
+                l1 = self.compiled_loss(z1, z2)
+                l2 = self.compiled_loss(z2, z1)
+            elif self.method == "barlow":
+                l1 = self.compiled_loss(z1, z2)
+                l2 = 0
 
-            l1 = self.compiled_loss(*l1_args)
-            l2 = self.compiled_loss(*l2_args)
             loss = l1 + l2
-
         # collect train variables from both the encoder and the projector
         tvars = self.backbone.trainable_variables
         tvars += self.projector.trainable_variables
@@ -203,7 +203,7 @@ class ContrastiveModel(tf.keras.Model):
         batch_size: Optional[int] = None,
         verbose: int = 0,
         steps: Optional[int] = None,
-        callbacks: Optional[tf.keras.Callback] = None,
+        callbacks: Optional[tf.keras.callbacks.Callback] = None,
         max_queue_size: int = 10,
         workers: int = 1,
         use_multiprocessing: bool = False,