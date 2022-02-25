4e36ec262235b119267821e9e1502c39605aafa4
dewball345 <abhiraamkumar@gmail.com>
2022-02-24 19:57:40 -0800

    Added test coverage for augmentation functions + barlow, simCLR augmenter  (#235)
    * Create test_blur.py
    
    * Create test_color_jitter.py
    
    * Create test_crop.py
    
    * Create test_flip.py
    
    * Update test_crop.py
    
    * Update test_color_jitter.py
    
    * Create test_solarize.py
    
    * Create test_augmenters.py
    
    * Update test_flip.py
    
    * Update test_flip.py
    
    * Update test_flip.py
    
    * Update blur.py
    
    * Update blur.py

---
+++ tests/augmenters/test_augmentation_utils/test_flip.py
@@ -0,0 +1,35 @@
+import pytest
+from tensorflow_similarity.augmenters.augmentation_utils import flip
+import tensorflow as tf 
+
+def create_img(width=32, height=32, channels=3):
+  return tf.random.uniform(
+    [width, height, channels], 0, 1)
+  
+def test_flip_left_right():
+  img = create_img()
+
+  random_flip_always = flip.random_random_flip_left_right(img, 1)
+  random_flip_never = flip.random_random_flip_left_right(img, 0)
+
+  # check shapes
+  assert (tf.shape(random_flip_always) == tf.shape(img)).numpy().all()
+  assert (tf.shape(random_flip_never) == tf.shape(img)).numpy().all()
+
+  # check if flip works
+#   assert not (random_flip_always == img).numpy().all()
+  assert (random_flip_never == img).numpy().all()
+
+def test_flip_top_bottom():
+  img = create_img()
+
+  random_flip_always = flip.random_random_flip_top_bottom(img, 1)
+  random_flip_never = flip.random_random_flip_top_bottom(img, 0)
+
+  # check shapes
+  assert (tf.shape(random_flip_always) == tf.shape(img)).numpy().all()
+  assert (tf.shape(random_flip_never) == tf.shape(img)).numpy().all()
+
+  # check if flip works 
+#   assert not (random_flip_always == img).numpy().all() # removing because would fail if flip were symmetric
+  assert (random_flip_never == img).numpy().all()
