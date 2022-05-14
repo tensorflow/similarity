86540f77f38453a768dd07d2f067984be1cf345f
dewball345 <abhiraamkumar@gmail.com>
2022-05-14 12:17:20 -0700

    Allow user to set normalization

--- tensorflow_similarity/augmenters/augmentation_utils/solarize.py
+++ tensorflow_similarity/augmenters/augmentation_utils/solarize.py
@@ -3,10 +3,13 @@ from tensorflow import Tensor
 from tensorflow_similarity.augmenters.augmentation_utils.random_apply import random_apply
 
 def random_solarize(
-    image: Tensor, p: float = 0.2, thresh: int = 10
+    image: Tensor, p: float = 0.2, thresh: int = 10, normalize: bool = True
 ) -> Tensor:
     def _transform(image: Tensor) -> Tensor:
-        return tf.where(image < 10/255, image, 255/255 - image)
+        if normalize:
+            return tf.where(image < 10/255, image, 255/255 - image)
+        else:
+            return tf.where(image < 10, image, 255 - image)
 
     return random_apply(_transform, p=p, x=image)
 
