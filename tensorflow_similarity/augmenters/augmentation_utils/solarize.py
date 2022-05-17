d1ed41867d938e003086f54399b5a53ad528c7e8
dewball345 <abhiraamkumar@gmail.com>
2022-05-16 18:20:44 -0700

    Update solarize.py

--- tensorflow_similarity/augmenters/augmentation_utils/solarize.py
+++ tensorflow_similarity/augmenters/augmentation_utils/solarize.py
@@ -3,13 +3,10 @@ from tensorflow import Tensor
 from tensorflow_similarity.augmenters.augmentation_utils.random_apply import random_apply
 
 def random_solarize(
-    image: Tensor, p: float = 0.2, thresh: int = 10, normalize: bool = True
+    image: Tensor, p: float = 0.2, pixel_min: 0, pixel_max: 255, thresh: int = 10
 ) -> Tensor:
     def _transform(image: Tensor) -> Tensor:
-        if normalize:
-            return tf.where(image < 10/255, image, 255/255 - image)
-        else:
-            return tf.where(image < 10, image, 255 - image)
+       return tf.where(image < thresh, image, pixel_max - image)
 
     return random_apply(_transform, p=p, x=image)
 
