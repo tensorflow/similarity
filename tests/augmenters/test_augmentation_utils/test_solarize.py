de72285c352339c53806a0ccc54b381504df0621
dewball345 <abhiraamkumar@gmail.com>
2022-05-17 12:02:43 -0700

    Update test_solarize.py

--- tests/augmenters/test_augmentation_utils/test_solarize.py
+++ tests/augmenters/test_augmentation_utils/test_solarize.py
@@ -9,7 +9,7 @@ def create_img(width=32, height=32, channels=3):
 def test_solarization():
   img = create_img()
 
-  random_solarize_always = solarize.random_solarize(img, p=1, thresh=1, pixel_min=0, pixel_max=1)
+  random_solarize_always = solarize.random_solarize(img, p=1, thresh=0, pixel_min=0, pixel_max=1)
   random_solarize_never = solarize.random_solarize(img, p=0, thresh=0, pixel_min=0, pixel_max=1)
 
   # check shapes
