Fix error when using retrieval metric 'macro' average.
tf.unique expects a 1D input but y.shape is (batch, 1). Add a reshape
before calling the tf.unique.

tensorflow_similarity/retrieval_metrics/bndcg.py
@@ -145,7 +145,7 @@ class BNDCG(RetrievalMetric):
             ndcg = tf.math.reduce_mean(per_example_ndcg)
         elif self.average == "macro":
             per_class_metrics = 0
-            class_labels = tf.unique(query_labels)[0]
+            class_labels = tf.unique(tf.reshape(query_labels, (-1)))[0]
             for label in class_labels:
                 idxs = tf.where(query_labels == label)
                 c_slice = tf.gather(per_example_ndcg, indices=idxs)

tensorflow_similarity/retrieval_metrics/recall_at_k.py
@@ -81,7 +81,7 @@ class RecallAtK(RetrievalMetric):
             recall_at_k = tf.math.reduce_mean(match_indicator)
         elif self.average == "macro":
             per_class_metrics = 0
-            class_labels = tf.unique(query_labels)[0]
+            class_labels = tf.unique(tf.reshape(query_labels, (-1)))[0]
             # TODO(ovallis): potential slowness.
             for label in class_labels:
                 idxs = tf.where(query_labels == label)