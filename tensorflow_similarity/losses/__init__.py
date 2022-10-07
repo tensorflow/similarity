5a80c3260422a76c745bd074e2cdf9a43442cc52
Owen Vallis <ovallis@google.com>
2022-10-07 09:38:34 -0700

    Update __init__.py

--- tensorflow_similarity/losses/__init__.py
+++ tensorflow_similarity/losses/__init__.py
@@ -16,12 +16,14 @@
 Contrastive learning specialized losses.
 """
 from .arcface_loss import ArcFaceLoss  # noqa
+from .barlow import Barlow  # noqa
 from .circle_loss import CircleLoss  # noqa
 from .metric_loss import MetricLoss  # noqa
 from .multisim_loss import MultiSimilarityLoss  # noqa
 from .pn_loss import PNLoss  # noqa
 from .simclr import SimCLRLoss  # noqa
 from .simsiam import SimSiamLoss  # noqa
+from .softnn_loss import SoftNearestNeighborLoss  # noqa
 from .triplet_loss import TripletLoss  # noqa
 from .vicreg import VicReg  # noqa
 from .xbm_loss import XBM  # noqa
