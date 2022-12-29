06e3f7e3cd5f9f42c6abdcf5c45484ff92100942
Owen Vallis <owensvallis@gmail.com>
2022-12-29 05:36:02 +0000

    Refactor the tests for the losses.
    * Use the tf.TestCase packages
    * Create utils for perfect and bad embedding examples.
    * Refactor Triplet and PN Loss to have a single margin param now with
      float | None. None will now set the soft_margin.
    * Replace basic tf.logsumexp with the TF sim stable logsumexp in the
      soft margin.
    * Fix bug in semi-hard mining where we don't have any valid negatives >
      max positive. Previously this defaulted to selecting the example in
      idx 0. We now take the negative that is closest to the maximal
      positive without going over, i.e., max(d(a,n)) <= max(d(a,p)).
    * Refactor the triplet loss tests.
    * Create a losses dir under tests.

--- tests/losses/test_old_loss_tests.py
+++ tests/losses/test_old_loss_tests.py
@@ -1,118 +1,15 @@
 import numpy as np
 import tensorflow as tf
 
-from tensorflow_similarity.losses import (
-    MultiNegativesRankLoss,
-    MultiSimilarityLoss,
-    PNLoss,
-    SoftNearestNeighborLoss,
-    TripletLoss,
-)
-
-# [triplet loss]
-from tensorflow_similarity.losses.xbm_loss import XBM
-
-
-def test_triplet_loss_serialization():
-    loss = TripletLoss()
-    config = loss.get_config()
-    print(config)
-    loss2 = TripletLoss.from_config(config)
-    assert loss.name == loss2.name
-    assert loss.distance == loss2.distance
-
-
-def triplet_hard_loss_np(labels, embedding, margin, dist_func, soft=False):
-
-    num_data = embedding.shape[0]
-    # Reshape labels to compute adjacency matrix.
-    labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
-
-    adjacency = np.equal(labels_reshaped, labels_reshaped.T)
-    pdist_matrix = dist_func(embedding)
-    loss_np = 0.0
-    for i in range(num_data):
-        pos_distances = []
-        neg_distances = []
-        for j in range(num_data):
-            if adjacency[i][j] == 0:
-                neg_distances.append(pdist_matrix[i][j])
-            if adjacency[i][j] > 0.0 and i != j:
-                pos_distances.append(pdist_matrix[i][j])
-
-        # if there are no positive pairs, distance is 0
-        if len(pos_distances) == 0:
-            pos_distances.append(0)
-
-        # Sort by distance.
-        neg_distances.sort()
-        min_neg_distance = neg_distances[0]
-        pos_distances.sort(reverse=True)
-        max_pos_distance = pos_distances[0]
-
-        if soft:
-            loss_np += np.log1p(np.exp(max_pos_distance - min_neg_distance))
-        else:
-            loss_np += np.maximum(0.0, max_pos_distance - min_neg_distance + margin)
-
-    loss_np /= num_data
-    return loss_np
-
-
-def test_triplet_loss():
-    num_inputs = 10
-    # y_true: labels
-    y_true = tf.random.uniform((num_inputs,), 0, 10, dtype=tf.int32)
-    # y_preds: embedding
-    y_preds = tf.random.uniform((num_inputs, 20), 0, 1)
-    tpl = TripletLoss()
-    # y_true, y_preds
-    loss = tpl(y_true, y_preds)
-    assert loss > 0.9
-
-
-def test_triplet_loss_easy():
-    num_inputs = 10
-    # y_true: labels
-    y_true = tf.random.uniform((num_inputs,), 0, 3, dtype=tf.int32)
-    # y_preds: embedding
-    y_preds = tf.random.uniform((num_inputs, 16), 0, 1)
-    tpl = TripletLoss(positive_mining_strategy="easy", negative_mining_strategy="easy")
-    # y_true, y_preds
-    loss = tpl(y_true, y_preds)
-    assert loss > 0
-
-
-def test_triplet_loss_semi_hard():
-    num_inputs = 10
-    # y_true: labels
-    y_true = tf.random.uniform((num_inputs,), 0, 3, dtype=tf.int32)
-    # y_preds: embedding
-    y_preds = tf.random.uniform((num_inputs, 16), 0, 1)
-    tpl = TripletLoss(positive_mining_strategy="easy", negative_mining_strategy="semi-hard")
-    # y_true, y_preds
-    loss = tpl(y_true, y_preds)
-    assert loss
-
-
-def test_triplet_loss_hard():
-    num_inputs = 10
-    # y_true: labels
-    y_true = tf.random.uniform((num_inputs,), 0, 3, dtype=tf.int32)
-    # y_preds: embedding
-    y_preds = tf.random.uniform((num_inputs, 16), 0, 1)
-    tpl = TripletLoss(positive_mining_strategy="hard", negative_mining_strategy="hard")
-    # y_true, y_preds
-    loss = tpl(y_true, y_preds)
-    assert loss
+from tensorflow_similarity import losses
 
 
 # [pn loss]
 def test_pn_loss_serialization():
-    loss = PNLoss()
+    loss = losses.PNLoss()
     config = loss.get_config()
     print(config)
-    loss2 = PNLoss.from_config(config)
+    loss2 = losses.PNLoss.from_config(config)
     assert loss.name == loss2.name
     assert loss.distance == loss2.distance
 
@@ -123,7 +20,7 @@ def test_np_loss():
     y_true = tf.random.uniform((num_inputs,), 0, 10, dtype=tf.int32)
     # y_preds: embedding
     y_preds = tf.random.uniform((num_inputs, 20), 0, 1)
-    pnl = PNLoss()
+    pnl = losses.PNLoss()
     # y_true, y_preds
     loss = pnl(y_true, y_preds)
     assert loss > 0.9
@@ -131,9 +28,9 @@ def test_np_loss():
 
 # [soft neasrest neighbor loss]
 def test_softnn_loss_serialization():
-    loss = SoftNearestNeighborLoss(distance="cosine", temperature=50)
+    loss = losses.SoftNearestNeighborLoss(distance="cosine", temperature=50)
     config = loss.get_config()
-    loss2 = SoftNearestNeighborLoss.from_config(config)
+    loss2 = losses.SoftNearestNeighborLoss.from_config(config)
     assert loss.name == loss2.name
     assert loss.distance == loss2.distance
     assert loss.temperature == loss2.temperature
@@ -150,7 +47,6 @@ def softnn_util(y_true, x, temperature=1):
     x = x.numpy()
     batch_size = y_true.shape[0]
     loss = 0
-    eps = 1e-9
     for i in range(batch_size):
         numerator = 0
         denominator = 0
@@ -175,7 +71,7 @@ def test_softnn_loss():
     x = tf.random.uniform((num_inputs, 20), 0, 1)
 
     temperature = np.random.uniform(0.1, 50)
-    softnn = SoftNearestNeighborLoss(temperature=temperature)
+    softnn = losses.SoftNearestNeighborLoss(temperature=temperature)
     loss = softnn(y_true, x)
     loss_check = softnn_util(y_true, x, temperature)
     loss_diff = loss.numpy() - loss_check
@@ -213,8 +109,8 @@ def test_xbm_loss():
     )
 
     distance = "cosine"
-    loss = MultiSimilarityLoss(distance=distance)
-    loss_nowarm = XBM(loss, memory_size=12, warmup_steps=0)
+    loss = losses.MultiSimilarityLoss(distance=distance)
+    loss_nowarm = losses.XBM(loss, memory_size=12, warmup_steps=0)
 
     # test enqueue
     loss_nowarm(labels1, embeddings1)
@@ -237,7 +133,7 @@ def test_xbm_loss():
     tf.assert_equal(loss_nowarm._y_true_memory, tf.concat([labels2, labels2], axis=0))
 
     # test warmup
-    loss_warm = XBM(loss, memory_size=12, warmup_steps=1)
+    loss_warm = losses.XBM(loss, memory_size=12, warmup_steps=1)
 
     loss_warm(labels1, embeddings1)
     assert loss_warm._y_pred_memory.numpy().shape == (0, embed_dim)
@@ -250,8 +146,8 @@ def test_xbm_loss():
 
 # [multiple negatives ranking loss]
 def test_multineg_rank_loss_serialization():
-    loss = MultiNegativesRankLoss(distance="inner_product")
+    loss = losses.MultiNegativesRankLoss(distance="inner_product")
     config = loss.get_config()
-    loss2 = MultiNegativesRankLoss.from_config(config)
+    loss2 = losses.MultiNegativesRankLoss.from_config(config)
     assert loss.name == loss2.name
     assert loss.distance == loss2.distance
---
+++ tests/losses/utils.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+
+def generate_perfect_test_batch():
+    """Generate a batch of embeddings and labels that will result in a perfect loss score."""
+    batch_size = 32
+
+    # y_true: labels
+    y_true = tf.range(0, batch_size, dtype=tf.int32)
+    y_true = tf.concat([y_true, y_true], axis=0)
+
+    # y_preds: embedding
+    y_preds = tf.one_hot(y_true, depth=batch_size, dtype=tf.float32)
+
+    y_true = tf.expand_dims(y_true, axis=1)
+    return y_true, y_preds
+
+
+def generate_bad_test_batch():
+    """Generate a batch of embeddings and labels that will result in a mismatch for all classes."""
+    batch_size = 32
+
+    # y_true: labels
+    y_true = tf.range(0, batch_size, dtype=tf.int32)
+
+    # y_preds: embedding
+    y_preds = tf.concat([y_true, y_true[::-1]], axis=0)
+    y_preds = tf.one_hot(y_preds, depth=batch_size, dtype=tf.float32)
+
+    y_true = tf.concat([y_true, y_true], axis=0)
+    y_true = tf.expand_dims(y_true, axis=1)
+    return y_true, y_preds
