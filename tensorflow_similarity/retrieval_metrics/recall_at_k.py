Fix casting to use the default floatx where possible to avoid type errors when training using mixed precision or float16.

tensorflow_similarity/retrieval_metrics/recall_at_k.py
@@ -81,7 +81,7 @@ class RecallAtK(RetrievalMetric):
         k_slice = match_mask[:, start_k : start_k + self.k]
 
         match_indicator = tf.math.reduce_any(k_slice, axis=1)
-        match_indicator = tf.cast(match_indicator, dtype="float")
+        match_indicator = tf.cast(match_indicator, dtype=tf.keras.backend.floatx())
 
         if self.average == "micro":
             recall_at_k = tf.math.reduce_mean(match_indicator)

tensorflow_similarity/visualization/confusion_matrix_viz.py
@@ -66,7 +66,7 @@ def confusion_matrix(
         y_true = tf.cast(y_true, dtype="int32")
 
         cm = tf.math.confusion_matrix(y_true, y_pred)
-        cm = tf.cast(cm, dtype="float")
+        cm = tf.cast(cm, dtype=tf.keras.backend.floatx())
         accuracy = tf.linalg.trace(cm) / tf.math.reduce_sum(cm)
         misclass = 1 - accuracy
 