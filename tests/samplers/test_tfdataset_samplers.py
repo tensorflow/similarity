70bee2f9ef492167d9c10e0338ac63a501f9e0fb
Abel Theodros <atheodros@google.com>
2023-03-10 19:31:07 +0000

    Refactor similiarity unit tests to Tensorflow TestCase, reduce usage of Numpy API

--- tests/samplers/test_tfdataset_samplers.py
+++ tests/samplers/test_tfdataset_samplers.py
@@ -1,21 +1,22 @@
-import pytest
+import tensorflow as tf 
 
 from tensorflow_similarity.samplers import TFDatasetMultiShotMemorySampler
 
 
-def test_basic():
-    dataset_name = "mnist"
-    sampler = TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=10)
-    batch = sampler.generate_batch(42)
-    assert batch[0].shape == (20, 28, 28, 1)
+class DatasetSamplersTest(tf.test.TestCase):
+    def test_basic(self):
+        dataset_name = "mnist"
+        sampler = TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=10)
+        batch = sampler.generate_batch(42)
+        self.assertEqual(batch[0].shape,(20, 28, 28, 1))
 
 
-def test_wrong_key():
-    dataset_name = "mnist"
+    def test_wrong_key(self):
+        dataset_name = "mnist"
 
-    # X
-    with pytest.raises(ValueError):
-        TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=4, x_key="error")
-    # Y
-    with pytest.raises(ValueError):
-        TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=4, y_key="error")
+        # X
+        with self.assertRaises(ValueError):
+            TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=4, x_key="error")
+        # Y
+        with self.assertRaises(ValueError):
+            TFDatasetMultiShotMemorySampler(dataset_name=dataset_name, classes_per_batch=4, y_key="error")
