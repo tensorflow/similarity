48df5ad79416121dee0da769bd8383a477d72c9a
Abel Theodros <atheodros@google.com>
2023-03-10 19:31:07 +0000

    Refactor similiarity unit tests to Tensorflow TestCase, reduce usage of Numpy API

--- tests/samplers/test_memory_samplers.py
+++ tests/samplers/test_memory_samplers.py
@@ -1,84 +1,89 @@
 import re
+import sys
 
-import pytest
 import tensorflow as tf
 
 from tensorflow_similarity.samplers import MultiShotMemorySampler, select_examples
 
 
-def test_valid_class_numbers():
+class MemorySamplersTest(tf.test.TestCase):
+
+  def test_valid_class_numbers(self):
     "Check that sampler properly detect if num_class requests >> class avail"
     y = tf.constant([1, 2, 3, 1, 2, 3, 1])
     x = tf.constant([10, 20, 30, 10, 20, 30, 10])
 
     class_per_batch = 42
+    with self.assertRaises(ValueError):
+      MultiShotMemorySampler(x=x, y=y, classes_per_batch=class_per_batch)
 
-    with pytest.raises(ValueError):
-        MultiShotMemorySampler(x=x, y=y, classes_per_batch=class_per_batch)
-
-
-@pytest.mark.parametrize("example_per_class", [2, 20])
-def test_select_examples(example_per_class):
+  def test_select_examples(self):
     """Test select_examples with various sizes.
 
     Users may sample with replacement when creating batches, so check that we
     can handle when elements per class is either less than or greater than the
     total count of elements in the class.
     """
-    y = tf.constant([1, 2, 3, 1, 2, 3, 1])
-    x = tf.constant([10, 20, 30, 10, 20, 30, 10])
-    cls_list = [1, 3]
-    batch_x, batch_y = select_examples(x, y, cls_list, example_per_class)
 
-    assert len(batch_y) == len(cls_list) * example_per_class
-    assert len(batch_x) == len(cls_list) * example_per_class
+    examples_per_class = (2, 20)
 
-    for x, y in zip(batch_x, batch_y):
-        assert y in cls_list
+    for example_per_class in examples_per_class:
+      y = tf.constant([1, 2, 3, 1, 2, 3, 1])
+      x = tf.constant([10, 20, 30, 10, 20, 30, 10])
+      cls_list = [1, 3]
+      with self.subTest(example_per_class=example_per_class):
+        batch_x, batch_y = select_examples(x, y, cls_list, example_per_class)
 
-        if y == 1:
-            assert x == 10
-        elif y == 3:
-            assert x == 30
+        self.assertLen(batch_y, len(cls_list) * example_per_class)
+        self.assertLen(batch_x, len(cls_list) * example_per_class)
 
+        for x, y in zip(batch_x, batch_y):
+          self.assertIn(y, cls_list)
 
-@pytest.mark.parametrize("example_per_class", [2, 20])
-def test_multi_shot_memory_sampler(example_per_class):
+          if y == 1:
+            self.assertEqual(x, 10)
+          elif y == 3:
+            self.assertEqual(x, 30)
+
+  def test_multi_shot_memory_sampler(self):
     """Test MultiShotMemorySampler with various sizes.
 
     Users may sample with replacement when creating batches, so check that we
     can handle when elements per class is either less than or greater than the
     total count of elements in the class.
     """
-    y = tf.constant([1, 2, 3, 1, 2, 3, 1])
-    x = tf.constant([10, 20, 30, 10, 20, 30, 10])
-    class_per_batch = 2
-    batch_size = example_per_class * class_per_batch
 
-    ms_sampler = MultiShotMemorySampler(
-        x=x,
-        y=y,
-        classes_per_batch=class_per_batch,
-        examples_per_class_per_batch=example_per_class,
-    )  # noqa
+    examples_per_class = (2, 20)
 
-    batch_x, batch_y = ms_sampler.generate_batch(batch_id=606)
+    for example_per_class in examples_per_class:
+      y = tf.constant([1, 2, 3, 1, 2, 3, 1])
+      x = tf.constant([10, 20, 30, 10, 20, 30, 10])
+      class_per_batch = 2
+      batch_size = example_per_class * class_per_batch
+      with self.subTest(example_per_class=example_per_class):
+        ms_sampler = MultiShotMemorySampler(
+            x=x,
+            y=y,
+            classes_per_batch=class_per_batch,
+            examples_per_class_per_batch=example_per_class,
+        )  # noqa
 
-    assert len(batch_y) == batch_size
-    assert len(batch_x) == batch_size
-    num_classes, _ = tf.unique(batch_y)
-    assert len(num_classes) == class_per_batch
+        batch_x, batch_y = ms_sampler.generate_batch(batch_id=606)
 
-    for x, y in zip(batch_x, batch_y):
-        if y == 1:
-            assert x == 10
-        elif y == 2:
-            assert x == 20
-        elif y == 3:
-            assert x == 30
+        self.assertLen(batch_y, batch_size)
+        self.assertLen(batch_x, batch_size)
+        num_classes, _ = tf.unique(batch_y)
+        self.assertLen(num_classes, class_per_batch)
 
+        for x, y in zip(batch_x, batch_y):
+          if y == 1:
+            self.assertEqual(x, 10)
+          elif y == 2:
+            self.assertEqual(x, 20)
+          elif y == 3:
+            self.assertEqual(x, 30)
 
-def test_msms_get_slice():
+  def test_msms_get_slice(self):
     """Test the multi shot memory sampler get_slice method."""
     y = tf.constant(range(4))
     x = tf.constant([[0] * 10, [1] * 10, [2] * 10, [3] * 10])
@@ -89,41 +94,40 @@ def test_msms_get_slice():
     ms_sampler._y = y
     slice_x, slice_y = ms_sampler.get_slice(1, 2)
 
-    assert slice_x.shape == (2, 10)
-    assert slice_y.shape == (2,)
+    self.assertEqual(slice_x.shape, (2, 10))
+    self.assertEqual(slice_y.shape, (2,))
 
-    assert slice_x[0, 0] == 1
-    assert slice_x[1, 0] == 2
+    self.assertEqual(slice_x[0, 0], 1)
+    self.assertEqual(slice_x[1, 0], 2)
 
-    assert slice_y[0] == 1
-    assert slice_y[1] == 2
+    self.assertEqual(slice_y[0], 1)
+    self.assertEqual(slice_y[1], 2)
 
-
-def test_msms_properties():
+  def test_msms_properties(self):
     """Test the multi shot memory sampler num_examples and shape"""
     y = tf.constant(range(4))
     x = tf.ones([4, 10, 20, 3])
 
     ms_sampler = MultiShotMemorySampler(x=x, y=y)
 
-    assert ms_sampler.num_examples == 4
-    assert ms_sampler.example_shape == (10, 20, 3)
+    self.assertEqual(ms_sampler.num_examples, 4)
+    self.assertEqual(ms_sampler.example_shape, (10, 20, 3))
 
-
-def test_small_class_size(capsys):
+  def test_small_class_size(self):
     """Test examples_per_class is > the number of class examples."""
     y = tf.constant([1, 1, 1, 2])
     x = tf.ones([4, 10, 10, 3])
 
-    ms_sampler = MultiShotMemorySampler(x=x, y=y, classes_per_batch=2, examples_per_class_per_batch=3)
+    with self.captureWritesToStream(sys.stdout) as captured:
+      ms_sampler = MultiShotMemorySampler(
+          x=x, y=y, classes_per_batch=2, examples_per_class_per_batch=3
+      )
+      _, batch_y = ms_sampler.generate_batch(0)
+      y, _, class_counts = tf.unique_with_counts(batch_y)
 
-    _, batch_y = ms_sampler.generate_batch(0)
+    self.assertAllEqual(tf.sort(y), tf.constant([1, 2]))
+    self.assertAllEqual(class_counts, tf.constant([3, 3]))
 
-    y, _, class_counts = tf.unique_with_counts(batch_y)
-    assert tf.math.reduce_all(tf.sort(y) == tf.constant([1, 2]))
-    assert tf.math.reduce_all(class_counts == tf.constant([3, 3]))
-
-    captured = capsys.readouterr()
     expected_msg = (
         "WARNING: Class 2 only has 1 unique examples, but "
         "examples_per_class is set to 3. The current batch will sample "
@@ -132,16 +136,16 @@ def test_small_class_size(capsys):
         "SingleShotMemorySampler()."
     )
 
-    match = re.search(expected_msg, captured.out)
-    assert bool(match)
+    match = re.search(expected_msg, captured.contents())
+    self.assertIsNotNone(match)
 
-    _, batch_y = ms_sampler.generate_batch(0)
+    with self.captureWritesToStream(sys.stdout) as captured:
+      _, batch_y = ms_sampler.generate_batch(0)
+      y, _, class_counts = tf.unique_with_counts(batch_y)
 
-    y, _, class_counts = tf.unique_with_counts(batch_y)
-    assert tf.math.reduce_all(tf.sort(y) == tf.constant([1, 2]))
-    assert tf.math.reduce_all(class_counts == tf.constant([3, 3]))
+    self.assertAllEqual(tf.sort(y), tf.constant([1, 2]))
+    self.assertAllEqual(class_counts, tf.constant([3, 3]))
 
     # Subsequent batch should produce the sampler warning.
-    captured = capsys.readouterr()
-    match = re.search(expected_msg, captured.out)
-    assert not bool(match)
+    match = re.search(expected_msg, captured.contents())
+    self.assertIsNone(match)
--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -1,11 +1,14 @@
 import numpy as np
+import tensorflow as tf
 
 from tensorflow_similarity.indexer import Indexer
 
 from . import DATA_DIR
 
 
-def test_calibration():
+class IndexerTest(tf.test.TestCase):
+
+  def test_calibration(self):
     # CALIB TEST
     SIZE = 20
 
@@ -15,16 +18,22 @@ def test_calibration():
 
     index = Indexer(3)
     index.batch_add(data["embeddings_idx"][:SIZE], labels=data["y_idx"][:SIZE])
-    calibration = index.calibrate(data["embeddings_cal"][:SIZE], data["y_cal"][:SIZE], thresholds_targets, verbose=1)
+    calibration = index.calibrate(
+        data["embeddings_cal"][:SIZE],
+        data["y_cal"][:SIZE],
+        thresholds_targets,
+        verbose=1,
+    )
     # assert 'vl' in cutpoints
-    assert "optimal" in calibration.cutpoints
-    assert "0.5" in calibration.cutpoints
-    assert len(calibration.thresholds["distance"]) == len(calibration.thresholds["value"])
-    assert index.is_calibrated
+    self.assertIn("optimal", calibration.cutpoints)
+    self.assertIn("0.5", calibration.cutpoints)
+    self.assertEqual(
+        len(calibration.thresholds["distance"]),
+        len(calibration.thresholds["value"]),
+    )
+    self.assertTrue(index.is_calibrated)
 
-
-def test_indexer_basic_flow():
-
+  def test_indexer_basic_flow(self):
     prediction = np.array([[1, 1, 2]], dtype="float32")
     embs = np.array([[1, 1, 3], [3, 1, 2]], dtype="float32")
 
@@ -36,16 +45,17 @@ def test_indexer_basic_flow():
     # lookup
     matches = indexer.single_lookup(prediction)
 
-    assert isinstance(matches, list)
-    assert matches[0].distance < 0.016
+    self.assertIsInstance(matches, list)
+    self.assertLess(matches[0].distance, 0.016)
+    self.assertIsNotNone(matches[0].embedding)
 
-    assert np.array_equal(matches[0].embedding, embs[0])
-    assert matches[0].label == 0
-    assert matches[0].data == "test"
+    self.assertIsNone(
+        np.testing.assert_array_equal(matches[0].embedding, embs[0])
+    )
+    self.assertEqual(matches[0].label, 0)
+    self.assertEqual(matches[0].data, "test")
 
-
-def test_indexer_batch_add():
-
+  def test_indexer_batch_add(self):
     prediction = np.array([[1, 1, 2]], dtype="float32")
     embs = np.array([[1, 1, 3], [3, 1, 2]], dtype="float32")
 
@@ -53,84 +63,78 @@ def test_indexer_batch_add():
 
     # index data
     indexer.batch_add(embs, [0, 1], data=["test", "test2"])
-    assert indexer.size() == 2
+    self.assertEqual(indexer.size(), 2)
     # check results
     matches = indexer.single_lookup(prediction)
 
-    assert isinstance(matches, list)
-    assert matches[0].distance < 0.016
+    self.assertIsInstance(matches, list)
+    self.assertLess(matches[0].distance, 0.016)
 
-    assert np.array_equal(matches[0].embedding, embs[0])
-    assert matches[0].label == 0
-    assert matches[0].data == "test"
+    self.assertIsNone(
+        np.testing.assert_array_equal(matches[0].embedding, embs[0])
+    )
+    self.assertEqual(matches[0].label, 0)
+    self.assertEqual(matches[0].data, "test")
 
-
-def test_multiple_add():
-
+  def test_multiple_add(self):
     # arrays of preds which contains a single embedding list(list(embedding))
     predictions = np.array([[[1, 1, 3]], [[3, 1, 2]]], dtype="float32")
 
     indexer = Indexer(3)
     indexer.add(predictions[0])
-    assert indexer.size() == 1
+    self.assertEqual(indexer.size(), 1)
 
     indexer.add(predictions[1])
-    assert indexer.size() == 2
+    self.assertEqual(indexer.size(), 2)
 
-
-def test_multiple_add_mix_data():
-
+  def test_multiple_add_mix_data(self):
     embs = np.array([[1, 1, 3], [3, 1, 2]], dtype="float32")
 
     indexer = Indexer(3)
     indexer.batch_add(embs)
-    assert indexer.size() == 2
+    self.assertEqual(indexer.size(), 2)
 
     indexer.batch_add(embs, data=embs)
-    assert indexer.size() == 4
+    self.assertEqual(indexer.size(), 4)
 
-
-def test_reload(tmp_path):
-
+  def test_reload(self):
     embs = np.array([[1, 1, 3], [3, 1, 2]], dtype="float32")
 
     indexer = Indexer(3)
     indexer.batch_add(embs, verbose=0)
-    assert indexer.size() == 2
+    self.assertEqual(indexer.size(), 2)
+
+    path = self.get_temp_dir()
 
     # save
-    path = tmp_path / "test_save_and_add/"
     indexer.save(path)
 
     # reload
     indexer2 = Indexer.load(path)
-    assert indexer2.size() == 2
+    self.assertEqual(indexer2.size(), 2)
 
     # add more
     indexer2.batch_add(embs, data=embs)
-    assert indexer2.size() == 4
+    self.assertEqual(indexer2.size(), 4)
 
-
-def test_uncompress_reload(tmp_path):
+  def test_uncompress_reload(self):
     "Ensure uncompressed index work"
 
     embs = np.array([[1, 1, 3], [3, 1, 2]], dtype="float32")
 
     indexer = Indexer(3)
     indexer.batch_add(embs, verbose=0)
-    assert indexer.size() == 2
+    self.assertEqual(indexer.size(), 2)
 
     # save
-    path = tmp_path / "test_save_and_add/"
+    path = self.get_temp_dir()
     indexer.save(path, compression=False)
 
     # reload
     indexer2 = Indexer.load(path)
-    assert indexer2.size() == 2
+    self.assertEqual(indexer2.size(), 2)
 
-
-def test_index_reset():
-
+  def test_index_reset(self):
     prediction = np.array([[1, 1, 2]], dtype="float32")
     embs = np.array([[1, 1, 3], [3, 1, 2], [3, 2, 3]], dtype="float32")
 
@@ -146,16 +150,16 @@ def test_index_reset():
     stats = indexer.stats()
 
     # check results
-    assert len(matches) == 3
-    assert len(matches[0].embedding) == 3
-    assert matches[0].label == 0
-    assert list(matches[0].embedding) == list(embs[0])
+    self.assertLen(matches, 3)
+    self.assertLen(matches[0].embedding, 3)
+    self.assertEqual(matches[0].label, 0)
+    self.assertEqual(list(matches[0].embedding), list(embs[0]))
 
     # reset
     indexer.reset()
     stats = indexer.stats()
-    assert stats["num_lookups"] == 0
-    assert stats["num_items"] == 0
+    self.assertEqual(stats["num_lookups"], 0)
+    self.assertEqual(stats["num_items"], 0)
 
     # do-over
     indexer.add([embs[0]], label=42)
@@ -164,16 +168,15 @@ def test_index_reset():
     matches = indexer.single_lookup(prediction)
     stats = indexer.stats()
 
-    assert len(matches) == 2
-    assert len(matches[0].embedding) == 3
-    assert matches[0].label == 42
-    assert matches[1].label == 43
-    assert list(matches[0].embedding) == list(embs[0])
-    assert list(matches[1].embedding) == list(embs[1])
-    assert stats["num_lookups"] == 1
+    self.assertLen(matches, 2)
+    self.assertLen(matches[0].embedding, 3)
+    self.assertEqual(matches[0].label, 42)
+    self.assertEqual(matches[1].label, 43)
+    self.assertAllEqual(matches[0].embedding, embs[0])
+    self.assertAllEqual(matches[1].embedding, embs[1])
+    self.assertEqual(stats["num_lookups"], 1)
 
-
-def test_indexer_batch_ops():
+  def test_indexer_batch_ops(self):
     NUM_ELTS = 100
     NUM_DIMS = 10
     K = 3
@@ -182,11 +185,10 @@ def test_indexer_batch_ops():
     indexer.batch_add(data)
     results = indexer.batch_lookup(data, k=K)
     indexer.stats()
-    assert len(results) == 100
-    assert len(results[0]) == K
+    self.assertLen(results, 100)
+    self.assertLen(results[0], K)
 
-
-def test_single_vs_batch_ops():
+  def test_single_vs_batch_ops(self):
     "ensure batch and single ops are consistent"
     NUM_ELTS = 100
     NUM_DIMS = 10
@@ -198,7 +200,13 @@ def test_single_vs_batch_ops():
 
     single_results = []
     for d in data:
-        single_results.append(indexer.single_lookup([d], k=K))
+      single_results.append(indexer.single_lookup([d], k=K))
 
     for idx in range(len(single_results)):
-        assert single_results[idx][0].label == batch_results[idx][0].label
+      self.assertEqual(
+          single_results[idx][0].label, batch_results[idx][0].label
+      )
+
+
+if __name__ == "__main__":
+  tf.test.main()
