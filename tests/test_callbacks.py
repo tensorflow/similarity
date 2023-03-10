611363b003f2b0390f12fd511570cd585f51777a
Abel Theodros <atheodros@google.comm>
2023-03-10 19:31:07 +0000

    Refactor similiarity unit tests to Tensorflow TestCase, reduce usage of Numpy API

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -9,7 +9,9 @@ from tensorflow_similarity.evaluators import MemoryEvaluator
 from tensorflow_similarity.models import SimilarityModel
 
 
-def test_eval_init_defaults():
+class CallbacksTest(tf.test.TestCase):
+
+  def test_eval_init_defaults(self):
     queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
@@ -22,20 +24,22 @@ def test_eval_init_defaults():
         target_labels=target_labels,
     )
 
-    assert tf.math.reduce_all(callback.queries_known == queries)
-    assert tf.math.reduce_all(callback.query_labels_known == query_labels)
-    assert tf.math.reduce_all(callback.targets == targets)
-    assert tf.math.reduce_all(callback.target_labels == target_labels)
-    assert callback.distance == "cosine"
-    assert isinstance(callback.evaluator, MemoryEvaluator)
-    assert {"binary_accuracy"} == set([m.name for m in callback.classification_metrics])
-    assert callback.k == 1
-    assert tf.math.reduce_all(callback.distance_thresholds == tf.constant([math.inf]))
-    assert callback.matcher == "match_nearest"
-    assert callback.tb_writer is None
+    self.assertAllEqual(callback.queries_known, queries)
+    self.assertAllEqual(callback.query_labels_known, query_labels)
+    self.assertAllEqual(callback.targets, targets)
+    self.assertAllEqual(callback.target_labels, target_labels)
+    self.assertEqual(callback.distance, "cosine")
+    self.assertIsInstance(callback.evaluator, MemoryEvaluator)
+    self.assertAllEqual(
+        {"binary_accuracy"},
+        set([m.name for m in callback.classification_metrics]),
+    )
+    self.assertEqual(callback.k, 1)
+    self.assertEqual(callback.distance_thresholds, tf.constant([math.inf]))
+    self.assertEqual(callback.matcher, "match_nearest")
+    self.assertIsNone(callback.tb_writer)
 
-
-def test_eval_init(tmp_path):
+  def test_eval_init(self):
     queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
@@ -46,9 +50,7 @@ def test_eval_init(tmp_path):
     matcher = "majority_vote"
     distance_thresholds = tf.constant([0.1, 0.2, 0.3])
 
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
+    log_dir = self.get_temp_dir()
 
     callback = EvalCallback(
         queries=queries,
@@ -63,28 +65,27 @@ def test_eval_init(tmp_path):
         tb_logdir=log_dir,
     )
 
-    assert tf.math.reduce_all(callback.queries_known == queries)
-    assert tf.math.reduce_all(callback.query_labels_known == query_labels)
-    assert tf.math.reduce_all(callback.targets == targets)
-    assert tf.math.reduce_all(callback.target_labels == target_labels)
-    assert callback.distance == "l2"
-    assert isinstance(callback.evaluator, MemoryEvaluator)
-    assert {"recall"} == set([m.name for m in callback.classification_metrics])
-    assert callback.k == 11
-    assert tf.math.reduce_all(callback.distance_thresholds == distance_thresholds)
-    assert callback.matcher == "majority_vote"
-    assert isinstance(callback.tb_writer, tf.summary.SummaryWriter)
+    self.assertAllEqual(callback.queries_known, queries)
+    self.assertAllEqual(callback.query_labels_known, query_labels)
+    self.assertAllEqual(callback.targets, targets)
+    self.assertAllEqual(callback.target_labels, target_labels)
+    self.assertEqual(callback.distance, "l2")
+    self.assertIsInstance(callback.evaluator, MemoryEvaluator)
+    self.assertEqual(
+        {"recall"}, set([m.name for m in callback.classification_metrics])
+    )
+    self.assertEqual(callback.k, 11)
+    self.assertAllEqual(callback.distance_thresholds, distance_thresholds)
+    self.assertEqual(callback.matcher, "majority_vote")
+    self.assertIsInstance(callback.tb_writer, tf.summary.SummaryWriter)
 
-
-def test_eval_callback(tmp_path):
+  def test_eval_callback(self):
     queries = tf.constant([[1.0, 2.0], [1.0, 2.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [1.0, 2.0]])
     target_labels = tf.constant([1, 2])
 
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
+    log_dir = self.get_temp_dir()
 
     callback = EvalCallback(
         queries=queries,
@@ -108,11 +109,12 @@ def test_eval_callback(tmp_path):
     callback.on_epoch_end(0, logs)
 
     metric_values = np.array(list(logs.values()))
-    assert {"binary_accuracy", "f1score"} == logs.keys()
-    np.testing.assert_allclose(np.array([0.5, 0.666667]), metric_values, rtol=1e-5, atol=0)
+    self.assertEqual({"binary_accuracy", "f1score"}, logs.keys())
+    np.testing.assert_allclose(
+        np.array([0.5, 0.666667]), metric_values, rtol=1e-5, atol=0
+    )
 
-
-def test_split_val_init_defaults():
+  def test_split_val_init_defaults(self):
     queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
@@ -131,23 +133,25 @@ def test_split_val_init_defaults():
         known_classes=known_classes,
     )
 
-    assert tf.math.reduce_all(callback.targets == targets)
-    assert tf.math.reduce_all(callback.target_labels == target_labels)
-    assert callback.distance == "cosine"
-    assert isinstance(callback.evaluator, MemoryEvaluator)
-    assert {"binary_accuracy"} == set([m.name for m in callback.classification_metrics])
-    assert callback.k == 1
-    assert tf.math.reduce_all(callback.distance_thresholds == tf.constant([math.inf]))
-    assert callback.matcher == "match_nearest"
-    assert callback.tb_writer is None
+    self.assertAllEqual(callback.targets, targets)
+    self.assertAllEqual(callback.target_labels, target_labels)
+    self.assertEqual(callback.distance, "cosine")
+    self.assertIsInstance(callback.evaluator, MemoryEvaluator)
+    self.assertAllEqual(
+        {"binary_accuracy"},
+        set([m.name for m in callback.classification_metrics]),
+    )
+    self.assertEqual(callback.k, 1)
+    self.assertEqual(callback.distance_thresholds, tf.constant([math.inf]))
+    self.assertEqual(callback.matcher, "match_nearest")
+    self.assertIsNone(callback.tb_writer)
 
-    assert tf.math.reduce_all(callback.queries_known == q_known)
-    assert tf.math.reduce_all(callback.query_labels_known == q_label_known)
-    assert tf.math.reduce_all(callback.queries_unknown == q_unknown)
-    assert tf.math.reduce_all(callback.query_labels_unknown == q_label_unknown)
+    self.assertAllEqual(callback.queries_known, q_known)
+    self.assertAllEqual(callback.query_labels_known, q_label_known)
+    self.assertAllEqual(callback.queries_unknown, q_unknown)
+    self.assertAllEqual(callback.query_labels_unknown, q_label_unknown)
 
-
-def test_split_val_init(tmp_path):
+  def test_split_val_init(self):
     queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
@@ -163,9 +167,7 @@ def test_split_val_init(tmp_path):
     q_unknown = tf.constant([[3.0, 4.0]])
     q_label_unknown = tf.constant([2], dtype="int32")
 
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
+    log_dir = self.get_temp_dir()
 
     callback = EvalCallback(
         queries=queries,
@@ -181,32 +183,109 @@ def test_split_val_init(tmp_path):
         tb_logdir=log_dir,
     )
 
-    assert tf.math.reduce_all(callback.targets == targets)
-    assert tf.math.reduce_all(callback.target_labels == target_labels)
-    assert callback.distance == "l2"
-    assert isinstance(callback.evaluator, MemoryEvaluator)
-    assert {"recall"} == set([m.name for m in callback.classification_metrics])
-    assert callback.k == 11
-    assert tf.math.reduce_all(callback.distance_thresholds == distance_thresholds)
-    assert callback.matcher == "majority_vote"
-    assert isinstance(callback.tb_writer, tf.summary.SummaryWriter)
+    self.assertAllEqual(callback.targets, targets)
+    self.assertAllEqual(callback.target_labels, target_labels)
+    self.assertEqual(callback.distance, "l2")
+    self.assertIsInstance(callback.evaluator, MemoryEvaluator)
+    self.assertEqual(
+        {"recall"}, set([m.name for m in callback.classification_metrics])
+    )
+    self.assertEqual(callback.k, 11)
+    self.assertAllEqual(callback.distance_thresholds, distance_thresholds)
+    self.assertEqual(callback.matcher, "majority_vote")
+    self.assertIsInstance(callback.tb_writer, tf.summary.SummaryWriter)
 
-    assert tf.math.reduce_all(callback.queries_known == q_known)
-    assert tf.math.reduce_all(callback.query_labels_known == q_label_known)
-    assert tf.math.reduce_all(callback.queries_unknown == q_unknown)
-    assert tf.math.reduce_all(callback.query_labels_unknown == q_label_unknown)
+    self.assertAllEqual(callback.queries_known, q_known)
+    self.assertAllEqual(callback.query_labels_known, q_label_known)
+    self.assertAllEqual(callback.queries_unknown, q_unknown)
+    self.assertEqual(callback.query_labels_unknown, q_label_unknown)
 
-
-def test_split_val_loss_callback(tmp_path):
+  def test_split_val_loss_callback(self):
     queries = tf.constant([[1.0, 2.0], [1.0, 2.0]])
     query_labels = tf.constant([1, 2])
     targets = tf.constant([[1.0, 2.0], [1.0, 2.0]])
     target_labels = tf.constant([1, 2])
     known_classes = tf.constant([1])
 
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
+    log_dir = self.get_temp_dir()
+
+    callback = EvalCallback(
+        queries=queries,
+        query_labels=query_labels,
+        targets=targets,
+        target_labels=target_labels,
+        known_classes=known_classes,
+        tb_logdir=str(log_dir),
+    )
+
+    # manually set model ^^
+    tf.keras.backend.clear_session()
+    inputs = tf.keras.layers.Input(shape=(2,))
+    outputs = tf.keras.layers.Dense(2)(inputs)
+    model = SimilarityModel(inputs, outputs)
+    model.compile("adam", loss="mse", distance="cosine")
+    callback.model = model
+
+    # call the only callback method implemented
+    callback.on_epoch_end(0, {})
+
+  def test_split_val_eval_init(self):
+    queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
+    query_labels = tf.constant([1, 2])
+    targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
+    target_labels = tf.constant([1, 2])
+    known_classes = tf.constant([1])
+    distance = "l2"
+    metrics = [Recall()]
+    k = 11
+    matcher = "majority_vote"
+    distance_thresholds = tf.constant([0.1, 0.2, 0.3])
+    q_known = tf.constant([[1.0, 2.0]])
+    q_label_known = tf.constant([1], dtype="int32")
+    q_unknown = tf.constant([[3.0, 4.0]])
+    q_label_unknown = tf.constant([2], dtype="int32")
+
+    log_dir = self.get_temp_dir()
+
+    callback = EvalCallback(
+        queries=queries,
+        query_labels=query_labels,
+        targets=targets,
+        target_labels=target_labels,
+        known_classes=known_classes,
+        distance=distance,
+        metrics=metrics,
+        k=k,
+        matcher=matcher,
+        distance_thresholds=distance_thresholds,
+        tb_logdir=log_dir,
+    )
+
+    self.assertAllEqual(callback.targets, targets)
+    self.assertAllEqual(callback.target_labels, target_labels)
+    self.assertEqual(callback.distance, "l2")
+    self.assertIsInstance(callback.evaluator, MemoryEvaluator)
+    self.assertEqual(
+        {"recall"}, set([m.name for m in callback.classification_metrics])
+    )
+    self.assertEqual(callback.k, 11)
+    self.assertAllEqual(callback.distance_thresholds, distance_thresholds)
+    self.assertEqual(callback.matcher, "majority_vote")
+    self.assertIsInstance(callback.tb_writer, tf.summary.SummaryWriter)
+
+    self.assertAllEqual(callback.queries_known, q_known)
+    self.assertAllEqual(callback.query_labels_known, q_label_known)
+    self.assertAllEqual(callback.queries_unknown, q_unknown)
+    self.assertAllEqual(callback.query_labels_unknown, q_label_unknown)
+
+  def test_split_val_eval_callback(self):
+    queries = tf.constant([[1.0, 2.0], [1.0, 2.0]])
+    query_labels = tf.constant([1, 2])
+    targets = tf.constant([[1.0, 2.0], [1.0, 2.0]])
+    target_labels = tf.constant([1, 2])
+    known_classes = tf.constant([1])
+
+    log_dir = self.get_temp_dir()
 
     callback = EvalCallback(
         queries=queries,
@@ -229,83 +308,5 @@ def test_split_val_loss_callback(tmp_path):
     callback.on_epoch_end(0, {})
 
 
-def test_split_val_eval_init(tmp_path):
-    queries = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-    query_labels = tf.constant([1, 2])
-    targets = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-    target_labels = tf.constant([1, 2])
-    known_classes = tf.constant([1])
-    distance = "l2"
-    metrics = [Recall()]
-    k = 11
-    matcher = "majority_vote"
-    distance_thresholds = tf.constant([0.1, 0.2, 0.3])
-    q_known = tf.constant([[1.0, 2.0]])
-    q_label_known = tf.constant([1], dtype="int32")
-    q_unknown = tf.constant([[3.0, 4.0]])
-    q_label_unknown = tf.constant([2], dtype="int32")
-
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
-
-    callback = EvalCallback(
-        queries=queries,
-        query_labels=query_labels,
-        targets=targets,
-        target_labels=target_labels,
-        known_classes=known_classes,
-        distance=distance,
-        metrics=metrics,
-        k=k,
-        matcher=matcher,
-        distance_thresholds=distance_thresholds,
-        tb_logdir=log_dir,
-    )
-
-    assert tf.math.reduce_all(callback.targets == targets)
-    assert tf.math.reduce_all(callback.target_labels == target_labels)
-    assert callback.distance == "l2"
-    assert isinstance(callback.evaluator, MemoryEvaluator)
-    assert {"recall"} == set([m.name for m in callback.classification_metrics])
-    assert callback.k == 11
-    assert tf.math.reduce_all(callback.distance_thresholds == distance_thresholds)
-    assert callback.matcher == "majority_vote"
-    assert isinstance(callback.tb_writer, tf.summary.SummaryWriter)
-
-    assert tf.math.reduce_all(callback.queries_known == q_known)
-    assert tf.math.reduce_all(callback.query_labels_known == q_label_known)
-    assert tf.math.reduce_all(callback.queries_unknown == q_unknown)
-    assert tf.math.reduce_all(callback.query_labels_unknown == q_label_unknown)
-
-
-def test_split_val_eval_callback(tmp_path):
-    queries = tf.constant([[1.0, 2.0], [1.0, 2.0]])
-    query_labels = tf.constant([1, 2])
-    targets = tf.constant([[1.0, 2.0], [1.0, 2.0]])
-    target_labels = tf.constant([1, 2])
-    known_classes = tf.constant([1])
-
-    log_dir = tmp_path / "sec/"
-    if not log_dir.exists():
-        log_dir.mkdir(parents=True)
-
-    callback = EvalCallback(
-        queries=queries,
-        query_labels=query_labels,
-        targets=targets,
-        target_labels=target_labels,
-        known_classes=known_classes,
-        tb_logdir=str(log_dir),
-    )
-
-    # manually set model ^^
-    tf.keras.backend.clear_session()
-    inputs = tf.keras.layers.Input(shape=(2,))
-    outputs = tf.keras.layers.Dense(2)(inputs)
-    model = SimilarityModel(inputs, outputs)
-    model.compile("adam", loss="mse", distance="cosine")
-    callback.model = model
-
-    # call the only callback method implemented
-    callback.on_epoch_end(0, {})
+if __name__ == "__main__":
+  tf.test.main()
