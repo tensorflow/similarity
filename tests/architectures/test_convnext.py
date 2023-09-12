2b7a553a7dbf0ef0b6fe94c84389a79075b5ffb0
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-09-12 10:08:57 -0300

    [lint] black on test file

--- tests/architectures/test_convnext.py
+++ tests/architectures/test_convnext.py
@@ -15,11 +15,13 @@ convneXt = pytest.importorskip("tensorflow_similarity.architectures.convnext")
 TF_MAJOR_VERSION = int(tf.__version__.split(".")[0])
 TF_MINOR_VERSION = int(tf.__version__.split(".")[1])
 
+
 def tf_version_check(major_version, minor_version):
     if TF_MAJOR_VERSION <= major_version and TF_MINOR_VERSION < minor_version:
         return True
 
     return False
+
 
 def test_build_convnext_tiny_full():
     input_layer = tf.keras.layers.Input((224, 224, 3))
@@ -35,12 +37,13 @@ def test_build_convnext_tiny_full():
         total_layer_count += 1
         if layer.trainable:
             trainable_layer_count += 1
-    
+
     expected_total_layer_count = 151
     expected_trainable_layer_count = 151
 
     assert total_layer_count == expected_total_layer_count
     assert trainable_layer_count == expected_trainable_layer_count
+
 
 def test_build_convnext_small_partial():
     input_layer = tf.keras.layers.Input((224, 224, 3))
@@ -63,6 +66,7 @@ def test_build_convnext_small_partial():
     assert total_layer_count == expected_total_layer_count
     assert trainable_layer_count == expected_trainable_layer_count
 
+
 def test_build_convnext_base_frozen():
     input_layer = tf.keras.layers.Input((224, 224, 3))
     output = convneXt.build_convnext("base", "imagenet", "frozen")(input_layer)
@@ -83,6 +87,7 @@ def test_build_convnext_base_frozen():
 
     assert total_layer_count == expected_total_layer_count
     assert trainable_layer_count == expected_trainable_layer_count
+
 
 def test_build_convnext_large_full():
     input_layer = tf.keras.layers.Input((224, 224, 3))
